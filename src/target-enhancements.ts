376602391f4966c0c9c45d77d39ff46d64eb5f5d
p4535992 <tentimarco0@gmail.com>
2021-01-16 12:31:48 +0100

    Update

--- src/target-enhancements.ts
+++ src/target-enhancements.ts
@@ -77,54 +77,68 @@ Hooks.once('ready', () => {
 
 // setup all the hooks
 
-Hooks.on("targetToken", () => { 
+Hooks.on("targetToken", TargetEnhancements.targetTokenEventHandler);
+Hooks.on("hoverToken", TargetEnhancements.hoverTokenEventHandler);
+Hooks.on("updateToken",TargetEnhancements.updateTokenEventHandler);
+Hooks.on("render",TargetEnhancements.renderTokenEventHandler);
+Hooks.on("preUpdateScene",TargetEnhancements.preUpdateSceneEventHandler);
+Hooks.on("renderSceneControls",TargetEnhancements.preUpdateSceneEventHandler);
+Hooks.on("controlToken",TargetEnhancements.controlTokenEventHandler);
+Hooks.on("clearTokenTargets",TargetEnhancements.clearTokenTargetsHandler);
+Hooks.on("getSceneControlButtons",TargetEnhancements.getSceneControlButtonsHandler);
+
+Hooks.on("canvasReady",TargetEnhancements.canvasReadyHandler);
+
+// TODO INTEGRATED LIB WRAPPER
+
+// Hooks.on("targetToken", () => { 
   
-  //libWrapper.register(MODULE_NAME, 'Token.prototype.setTarget', TargetEnhancements.targetTokenEventHandler, 'WRAPPER');
-  TargetEnhancements.targetTokenEventHandler
-});
+//   //libWrapper.register(MODULE_NAME, 'Token.prototype.setTarget', TargetEnhancements.targetTokenEventHandler, 'WRAPPER');
+//   TargetEnhancements.targetTokenEventHandler
+// });
 
-Hooks.on("hoverToken", () => {
-  TargetEnhancements.hoverTokenEventHandler
-});
+// Hooks.on("hoverToken", () => {
+//   TargetEnhancements.hoverTokenEventHandler
+// });
 
-Hooks.on("updateToken", () => {
+// Hooks.on("updateToken", () => {
 
-  //libWrapper.register(MODULE_NAME, 'Token.prototype.update', TargetEnhancements.renderTokenEventHandler, 'WRAPPER');
-  TargetEnhancements.renderTokenEventHandler
-});
+//   //libWrapper.register(MODULE_NAME, 'Token.prototype.update', TargetEnhancements.renderTokenEventHandler, 'WRAPPER');
+//   TargetEnhancements.renderTokenEventHandler
+// });
 
-Hooks.on("render", () => {
+// Hooks.on("render", () => {
 
-  //libWrapper.register(MODULE_NAME, 'TokenConfig.render', TargetEnhancements.renderTokenEventHandler, 'WRAPPER');
-  TargetEnhancements.renderTokenEventHandler
-});
-Hooks.on("preUpdateScene", () => {
+//   //libWrapper.register(MODULE_NAME, 'TokenConfig.render', TargetEnhancements.renderTokenEventHandler, 'WRAPPER');
+//   TargetEnhancements.renderTokenEventHandler
+// });
+// Hooks.on("preUpdateScene", () => {
 
-  TargetEnhancements.preUpdateSceneEventHandler
+//   TargetEnhancements.preUpdateSceneEventHandler
   
-});
-Hooks.on("renderSceneControls", () => {
+// });
+// Hooks.on("renderSceneControls", () => {
 
-  TargetEnhancements.preUpdateSceneEventHandler
+//   TargetEnhancements.preUpdateSceneEventHandler
   
-});
-Hooks.on("controlToken", () => {
+// });
+// Hooks.on("controlToken", () => {
 
-  //libWrapper.register(MODULE_NAME, 'Token.prototype.control', TargetEnhancements.controlTokenEventHandler, 'WRAPPER');
-  TargetEnhancements.controlTokenEventHandler
-});
-Hooks.on("clearTokenTargets", () => {
+//   //libWrapper.register(MODULE_NAME, 'Token.prototype.control', TargetEnhancements.controlTokenEventHandler, 'WRAPPER');
+//   TargetEnhancements.controlTokenEventHandler
+// });
+// Hooks.on("clearTokenTargets", () => {
 
-  //libWrapper.register(MODULE_NAME, 'TokenLayer.prototype.targetObjects', TargetEnhancements.clearTokenTargetsHandler, 'WRAPPER');
-  TargetEnhancements.clearTokenTargetsHandler
-});
-Hooks.on("getSceneControlButtons", () => {
+//   //libWrapper.register(MODULE_NAME, 'TokenLayer.prototype.targetObjects', TargetEnhancements.clearTokenTargetsHandler, 'WRAPPER');
+//   TargetEnhancements.clearTokenTargetsHandler
+// });
+// Hooks.on("getSceneControlButtons", () => {
 
-  TargetEnhancements.getSceneControlButtonsHandler
+//   TargetEnhancements.getSceneControlButtonsHandler
   
-});
-Hooks.on("canvasReady", () => {
+// });
+// Hooks.on("canvasReady", () => {
 
-  //libWrapper.register(MODULE_NAME, 'Canvas.prototype.ready',TargetEnhancements.canvasReadyHandler, 'WRAPPER');
-  TargetEnhancements.canvasReadyHandler
-});
+//   //libWrapper.register(MODULE_NAME, 'Canvas.prototype.ready',TargetEnhancements.canvasReadyHandler, 'WRAPPER');
+//   TargetEnhancements.canvasReadyHandler
+// });
